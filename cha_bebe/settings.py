23df4af3901413c9c50e73bd305ade165c81001b
intelektos <josuecalves@gmail.com>
2020-05-17 14:05:01 -0300

    config heroku

--- cha_bebe/settings.py
+++ cha_bebe/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -150,3 +152,4 @@ CONTACT_EMAIL = 'contato@simplecoo.com'
 #LOGOUT_URL = 'accounts:logout'
 #AUTH_USER_MODEL = 'accounts.User'
 
+django_heroku.settings(locals())
